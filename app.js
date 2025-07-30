041dc108ba984a8464672fa7ef63c2eca593c440
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2025-07-29 20:33:45 -0400

    feat: finish part 3 of the course

--- app.js
+++ app.js
@@ -1,27 +1,59 @@
-// let title = document.querySelector("h1");
-// let paragraph = document.querySelector("p");
+showDefaultText();
 
-// title.innerHTML = "Jogo do Número Secreto";
-// paragraph.innerHTML = "Escolha um número entre 1 e 10";
-
-puts("h1", "Jogo do Número Secreto");
-puts("p", "Escolha um número entre 1 e 10");
-
-let clicks = 0;
 let secretNumber = generateRandomNumber();
+let tentatives = 1;
 
-function verificarChute() {
+function checkGuess() {
   let guess = document.querySelector("input").value;
 
-  console.log(guess == secretNumber);
-  clicks++;
+  if (guess > secretNumber) {
+    putsHTML("p", "O número secreto é menor");
+  } else if (guess < secretNumber) {
+    putsHTML("p", "O número secreto é maior");
+  } else {
+    let tentativesText = tentatives == 1 ? "tentativa" : "tentativas";
+
+    putsHTML("h1", "Acertou!");
+    putsHTML(
+      "p",
+      `Você descobriu o número secreto com ${tentatives} ${tentativesText}`
+    );
+
+    document.querySelector("#reiniciar").removeAttribute("disabled");
+  }
+  tentatives++;
+  cleanField();
 }
 
-function puts(tag, text) {
+function newGame() {
+  secretNumber = generateRandomNumber();
+  tentatives = 1;
+  cleanField();
+
+  showDefaultText();
+
+  document.querySelector("#reiniciar").setAttribute("disabled", true);
+}
+
+function putsHTML(tag, text) {
   let field = document.querySelector(tag);
   field.innerHTML = text;
 }
 
-function generateRandomNumber(params) {
+function putsValue(tag, text) {
+  let field = document.querySelector(tag);
+  field.value = text;
+}
+
+function generateRandomNumber() {
   return parseInt(Math.random() * 10 + 1);
+}
+
+function cleanField() {
+  putsValue("input", "");
+}
+
+function showDefaultText() {
+  putsHTML("h1", "Jogo do Número Secreto");
+  putsHTML("p", "Escolha um número entre 1 e 10");
 }
